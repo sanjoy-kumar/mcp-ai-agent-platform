90356364885f52ab9ed4666ef7b113d90d4943f0
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2026-03-23 00:42:01 -0400

    feat: Implement Weather Tool

--- tools/index.js
+++ tools/index.js
@@ -1,16 +1,19 @@
 import * as calculate from './calculate.js';
 import * as health from './health.js';
 import * as db from './db.js';
+import * as weather from './weather.js';
 
 export const toolDefinitions = [
     calculate.definition,
     health.definition,
     db.definition,
+    weather.definition,
 ];
 
 export const toolHandlers = {
     calculate: calculate.handler,
     health_check: health.handler,
     query_db: db.handler,
+    get_weather: weather.handler,
 };
 
